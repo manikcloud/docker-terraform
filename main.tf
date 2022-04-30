586cc7712bc0482d1be36dc39268626ed7257594
VARUN KUMAR MANIK <varunmanik1@gmail.com>
2022-04-30 21:52:00 +0800

    Update main.tf
    adding httpd

--- main.tf
+++ main.tf
@@ -22,3 +22,19 @@ resource "docker_container" "nginx" {
     external = 8000
   }
 }
+
+###### HTTPD Container
+
+resource "docker_image" "httpd" {
+  name         = "httpd:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "httpd1" {
+  image = docker_image.httpd.latest
+  name  = "sl-tutorial-httpd"
+  ports {
+    internal = 80
+    external = 8001
+  }
+}
